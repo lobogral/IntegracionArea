570ac1353698cc8dba4550dd7e7399254f8dc6bd
Camilo Sabogal <lobogral@gmail.com>
2021-08-19 00:26:11 -0500

    Simplifico programa

--- Programa.py
+++ Programa.py
@@ -1,25 +1,18 @@
-from sympy import integrate, Piecewise, oo
+from sympy import integrate, Piecewise, oo, E
 from sympy import Symbol, And
-from sympy.functions import exp
 
 x = Symbol("x", real=True)
 y = Symbol("y", real=True)
 
-def __crearFuncionTrozos(función, *intervalos):
-    lista = [] 
-    lista += [(función, And(*intervalos))]
-    lista += [(0, True)]
-    return Piecewise(*lista)
-
 def Integral(función, *intervalos):
-    funcionTrozos = __crearFuncionTrozos(función, *intervalos)
+    funcionTrozos = Piecewise((función, And(*intervalos)),(0, True))
     variables = funcionTrozos.atoms(Symbol)
     lista = [(variable, -oo, oo) for variable in variables]
-    return integrate(funcionTrozos, *lista)
+    return integrate(funcionTrozos, *lista).simplify()
 
 v1 = (x<1)
 v2 = (0<y)
 v3 = (y<x)
-funcion = exp(-x**2)
+funcion = E**(-(x**2))
 
 print(Integral(funcion, v1, v2, v3))
