b5ec3927b7fdf0e3a7773bee6d72b8c3130b89b2
Camilo Sabogal <lobogral@gmail.com>
2021-09-13 15:54:23 -0500

    Separo funcion de aplicacion

--- src/Transformacion.py
+++ src/Transformacion.py
@@ -1,25 +1,27 @@
-from sympy import Matrix, Abs, Rational
+from sympy import Matrix, Abs
 from sympy.abc import x, y, u, v
 from Programa import Integral
+from sympy import Rational as R
 
-r1 = (x - 2*y <= 0)
-r2 = (x - 2*y >= -4)
-r3 = (x + y >= 1)
-r4 = (x + y <= 4)
+def Transformar(func, listRels, varsRem, diccRem):
 
+    M1 = Matrix(list(diccRem.values()))
+    M2 = Matrix(varsRem)
+    det = M1.jacobian(M2).det().simplify()
+
+    funcRem = Abs(det)*func.subs(diccRem)
+    listRelsRem = [rel.subs(diccRem) for rel in listRels]
+
+    return (funcRem, listRelsRem)    
+
+
+listRels = [x-2*y <= 0, x-2*y >= -4, x+y >= 1, x+y <= 4]
 func = 3*x*y
 
-dicc = {x:Rational(1,3)*(2*u + v),
-        y:Rational(1,3)*(u - v)}
+varsRem = [u, v]
+diccRem = {x:R(1,3)*(2*u + v),
+           y:R(1,3)*(u - v)}
 
-r1 = r1.subs(dicc)
-r2 = r2.subs(dicc)
-r3 = r3.subs(dicc)
-r4 = r4.subs(dicc)
-func = func.subs(dicc)
+funcRem, listRelsRem = Transformar(func, listRels, varsRem, diccRem)
 
-M1 = Matrix(list(dicc.values()))
-M2 = Matrix([u, v])
-det = M1.jacobian(M2).det().simplify()
-
-print(Integral(func*Abs(det), r1, r2, r3, r4))
+print(Integral(funcRem, *listRelsRem))
