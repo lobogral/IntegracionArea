0b8d84570cfdd192d1938a8d831d313875a900ba
Camilo Sabogal <lobogral@gmail.com>
2021-11-02 18:34:47 -0500

    Mejoro ligeramente la calidad

--- src/ejemplos.py
+++ src/ejemplos.py
@@ -10,9 +10,9 @@ v = Symbol("v")
 r = Symbol("r")
 
 # Ejemplo 1
-v1 = (x<1)
-v2 = (0<y)
-v3 = (y<x)
+v1 = (x < 1)
+v2 = (y > 0)
+v3 = (y < x)
 funcion = E**(-(x**2))
 
 print(integral(funcion, v1, v2, v3))
@@ -22,8 +22,8 @@ list_rels = [x-2*y <= 0, x-2*y >= -4, x+y >= 1, x+y <= 4]
 func = 3*x*y
 
 vars_reem = [u, v]
-dicc_reem = {x:R(1,3)*(2*u + v),
-             y:R(1,3)*(u - v)}
+dicc_reem = {x: R(1, 3)*(2*u + v),
+             y: R(1, 3)*(u - v)}
 
 det, func_reem, list_rels_reem = transformar(func, list_rels,
                                              vars_reem, dicc_reem)
@@ -34,11 +34,10 @@ list_rels = [x**2 + y**2 >= 1, x**2 + y**2 <= 5]
 func = x**2 + y
 
 vars_reem = [r, θ]
-dicc_reem = {x:r*cos(θ),
-             y:r*sin(θ)}
+dicc_reem = {x: r*cos(θ),
+             y: r*sin(θ)}
 
 det, func_reem, list_rels_reem = transformar(func, list_rels,
                                              vars_reem, dicc_reem)
 list_rels_reem += [r >= 0, θ >= 0, θ <= 2*pi]
 print(integral(det*func_reem, *list_rels_reem))
-
--- src/programa.py
+++ src/programa.py
@@ -2,20 +2,21 @@ from sympy import Matrix
 from sympy import integrate, Piecewise, oo
 from sympy import Symbol, And, simplify
 
+
 def integral(funcion, *intervalos):
-    funcion_trozos = Piecewise((funcion, And(*intervalos)),(0, True))
+    funcion_trozos = Piecewise((funcion, And(*intervalos)), (0, True))
     variables = funcion_trozos.atoms(Symbol)
     lista = [(variable, -oo, oo) for variable in variables]
     return integrate(funcion_trozos, *lista).simplify()
 
+
 def transformar(func, list_rels, vars_reem, dicc_reem):
 
-    M1 = Matrix(list(dicc_reem.values()))
-    M2 = Matrix(vars_reem)
-    det = M1.jacobian(M2).det().simplify()
+    matrix_1 = Matrix(list(dicc_reem.values()))
+    matrix_2 = Matrix(vars_reem)
+    det = matrix_1.jacobian(matrix_2).det().simplify()
 
     func_reem = func.subs(dicc_reem)
     list_rels_reem = [simplify(rel.subs(dicc_reem)) for rel in list_rels]
 
-    return (det, func_reem, list_rels_reem)    
-
+    return (det, func_reem, list_rels_reem)
